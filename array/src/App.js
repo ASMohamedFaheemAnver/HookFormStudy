04c823ae048db74e2099529e25623ad7bb888e35
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-04-09 16:35:12 +0530

    useFieldArray demo,

--- array/src/App.js
+++ array/src/App.js
@@ -1,7 +1,43 @@
 import "./App.css";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 
 function App() {
-  return <div></div>;
+  const { register, control, handleSubmit, reset, trigger, setError } = useForm(
+    {
+      // defaultValues: {}; you can populate the fields by this attribute
+    }
+  );
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "test",
+  });
+
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <ul>
+        {fields.map((item, index) => (
+          <li key={item.id}>
+            <input {...register(`test.${index}.firstName`)} />
+            <Controller
+              render={({ field }) => <input {...field} />}
+              name={`test.${index}.lastName`}
+              control={control}
+            />
+            <button type="button" onClick={() => remove(index)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <button
+        type="button"
+        onClick={() => append({ firstName: "", lastName: "" })}
+      >
+        append
+      </button>
+      <input type="submit" />
+    </form>
+  );
 }
 
 export default App;
